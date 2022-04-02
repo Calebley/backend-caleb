f7b55d2b8615d9887f36883a79fb50c1c072c752
Caleb <calebley@hotmail.com>
2022-04-02 15:55:06 +0800

    add holidaylist

--- controllers/holidayController.js
+++ controllers/holidayController.js
@@ -1,35 +1,42 @@
-const express = require("express")
-const router = express.Router()
+const express = require('express');
 const Holiday = require("../models/Holiday")
+const router = express.Router();
 
-router.get("/seed", async (req,res) => {
+router.get("/seed", async (req, res) => {
 
-    const holidays = [
-        {
-            name: "New Year's Day"
-        },
-        {
-            name: "Good friday"
-        }
-    ]
-    await Holiday.deleteMany({})
-    await Holiday.insertMany(holidays)
-    res.json(holidays)
+  const holidays = [
+    {
+      name: "New Year's Day",
+    },
+    {
+      name: "Good Friday",
+    }
+  ]
+  await Holiday.deleteMany({});
+  await Holiday.insertMany(holidays)
+  res.json(holidays)
 })
 
-router.get("/", (req,res) => {
-    res.send("holidays")
-})
+router.get('/', (req, res) => {
+    Holiday.find()
+      .then(holidays => {
+        res.json(holidays)
+      })
+      .catch(err => {
+        res.json(err)
+      })
+  })
 
-//Create route
+
+//* Create Route
 router.post("/", async (req, res) => {
-    console.log("body", req.body)
-    try {
-        const createdHoliday = await Holiday.create(req.body);
-        res.status(200).send(createdHoliday);
-      } catch (error) {
-        res.status(400).json({ error: error.message });
-      };
-})
+  console.log("body", req.body)
+  try {
+    const createdHoliday = await Holiday.create(req.body);
+    res.status(200).send(createdHoliday);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  };
+});
 
-module.exports = router
+module.exports = router;
