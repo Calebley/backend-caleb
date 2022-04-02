cc2bc2a1ef758e38d34512ecbc746f68803d7818
Caleb <calebley@hotmail.com>
2022-04-02 10:48:42 +0800

    add mongoose

---
+++ models/Holiday.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose")
+const { Schema } = mongoose
+
+const holidaySchema = new Schema({
+    name: String,
+    description: String,
+    likes: Number,
+    celebrated: Boolean
+})
+
+const Holiday = mongoose.model("Holiday", holidaySchema)
+module.exports = Holiday
--- server.js
+++ server.js
@@ -1,7 +1,27 @@
 require("dotenv").config()
 const express = require("express")
+const mongoose = require("mongoose")
+const Holiday = require("./models/Holiday")
+const HolidayController = require("./controllers/holidayController")
 const app = express()
 const PORT = process.env.PORT
+const MONGODB_URI = process.env.MONGODB_URI
+
+mongoose.connection.on("error", (err) =>
+  console.log(err.message + " is Mongod not running?")
+);
+mongoose.connection.on("disconnected", () => console.log("mongo disconnected"));
+
+//...farther down the page
+
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+});
+mongoose.connection.once("open", () => {
+  console.log("connected to mongoose...");
+});
+
+app.use("/api/holidays", HolidayController)
 
 app.get("/", (req,res) => {
     res.send("hi")
