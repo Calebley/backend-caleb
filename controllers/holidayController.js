86a7740331a916b0c738bce26b60bffec5e60968
Caleb <calebley@hotmail.com>
2022-04-02 11:01:33 +0800

    seeded

--- controllers/holidayController.js
+++ controllers/holidayController.js
@@ -1,5 +1,19 @@
 const express = require("express")
 const router = express.Router()
+const Holiday = require("../models/Holiday")
+
+router.get("/seed", async (req,res) => {
+    await Holiday.deleteMany({})
+    await Holiday.insertMany([
+        {
+            name: "New Year's Day"
+        },
+        {
+            name: "Good fridy"
+        }
+    ])
+    res.send("holidays seeded")
+})
 
 router.get("/", (req,res) => {
     res.send("holidays")
